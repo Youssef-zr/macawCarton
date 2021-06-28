4daf45c5051b0067b1364cba7dc4880958a50460
youssefzr <yn-neinaa@hotmail.com>
2021-06-28 16:32:33 +0100

    add slide up btn

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
-$(()=>{
+$(() => {
+
+    let slideUpBTn = $('.slide-up');
+
+    $(window).scroll(() => {
+        checkSlideUp();
+    })
+
+    function checkSlideUp() {
+
+        if ($('html,body').scrollTop() > 600) {
+            slideUpBTn.fadeIn(700)
+        } else {
+            slideUpBTn.fadeOut(700)
+        }
+    }
+
+    checkSlideUp();
+
+    slideUpBTn.click(() => {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700)
+    })
 })
